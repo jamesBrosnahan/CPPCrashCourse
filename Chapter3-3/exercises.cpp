b9d1ee9230ac1dfbb3142b1de0f2c742e9f6b8c0
James Brosnahan <jb172@zips.uakron.edu>
2020-02-14 21:59:26 -0500

    3-3 Doubly linked list back iteration does not print stormtrooper 1

--- Chapter3-3/exercises.cpp
+++ Chapter3-3/exercises.cpp
@@ -27,10 +27,52 @@ struct Element {
 		new_element->next = next;
 		next = new_element;
 	}
+	void insert_before(Element* new_element) {
+		new_element->previous = previous;
+		previous = new_element;
+	}
 	char prefix[2];
 	short operating_number;
 };
 
 int main() {
+	Element trooper1, trooper2, trooper3;
 
+	trooper1.prefix[0] = 'T';
+	trooper1.prefix[1] = 'K';
+	trooper1.operating_number = 421;
+	
+	trooper2.prefix[0] = 'F';
+	trooper2.prefix[1] = 'N';
+	trooper2.operating_number = 2187;
+
+	trooper3.prefix[0] = 'L';
+	trooper3.prefix[1] = 'S';
+	trooper3.operating_number = 005;
+	
+	trooper1.insert_after(&trooper2);
+	trooper2.insert_after(&trooper3);
+	
+	trooper2.insert_before(&trooper1);
+	trooper3.insert_before(&trooper2);
+
+	for (Element *cursor = &trooper1; cursor; cursor = cursor->next) {
+		printf("stormtrooper %c%c-%d\n", cursor->prefix[0], cursor->prefix[1], cursor->operating_number);
+	}
+
+	for (Element *cursor = &trooper3; cursor; cursor = cursor->previous) {
+		printf("stormtrooper %c%c-%d\n", cursor->prefix[0], cursor->prefix[1], cursor->operating_number);
+	}
+	
+	return 0;
 }
+
+
+
+
+
+
+
+
+
+
